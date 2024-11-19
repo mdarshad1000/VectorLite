bcd081c1170976ed865a227b02eef55420123cde
Mohammad Arshad <mdarshad1000@gmail.com>
2024-11-20 03:53:49 +0530

    feature: basic HNSW index

--- index/hnsw_index.py
+++ index/hnsw_index.py
@@ -1,14 +1,14 @@
 import hnswlib
 import numpy as np
 from typing import List, Dict
-from abstract_index import AbstractIndex
+from index.abstract_index import AbstractIndex
 
 
 class HNSWIndex(AbstractIndex):
     '''
     A class for implementing the Hierarchical Navigable Small World (HNSW) index for efficient vector search.
 
-    This class supports adding embeddings and performing similarity search using the HNSW algorithm. It is designed
+    This class supports adding data and performing similarity search using the HNSW algorithm. It is designed
     to be used with a large number of embeddings and supports incremental updates to the index.
 
     Attributes:
@@ -19,27 +19,15 @@ class HNSWIndex(AbstractIndex):
         metadatas (List[Dict], optional): Metadata associated with each embedding.
         M (int, optional): The number of links in the HNSW graph. Default is 16.
         ef_construction (int, optional): The number of nearest neighbors to consider during the graph construction. Default is 100.
-        metric (str, optional): The distance metric to use for the HNSW algorithm. Default is "cosine".
+        metric (str, optional): The distance metric to use for the HNSW algorithm. Default is "cosine". Other possible options are 'l2' and 'ip'
     '''
-    def __init__(
-        self,
-        table_name: str,
-        dimension: int,
-        ids: list,
-        embeddings: np.array,
-        metadatas: List[Dict] = None,
-        M: int = 16,
-        ef_construction: int = 100,
-        metric: str = "cosine",
-    ):
+    def __init__(self, table_name: str, dimension: int, ids: list, embeddings: np.array, metadatas: List[Dict] = None, M: int = 16, ef_construction: int = 100, metric: str = "cosine"):
         super().__init__(table_name, "HNSW", dimension, ids, embeddings)
 
         if not isinstance(embeddings, (np.ndarray, list)):
             raise ValueError("Embeddings should be a NumPy array or a list.")
 
-        self.embeddings = (
-            embeddings if isinstance(embeddings, np.ndarray) else np.array(embeddings)
-        )
+        self.embeddings = (embeddings if isinstance(embeddings, np.ndarray) else np.array(embeddings))
         self.dimension = dimension
 
         if self.embeddings.ndim == 1:
@@ -49,15 +37,67 @@ class HNSWIndex(AbstractIndex):
             raise ValueError("Embeddings should be a 1D or 2D array.")
 
         if self.embeddings.shape[1] != self.dimension:
-            raise ValueError(
-                f"Embeddings dimension does not match index dimension i.e {self.dimension}"
-            )
+            raise ValueError(f"Embeddings dimension does not match index dimension i.e {self.dimension}")
 
         self.table_name = table_name
         self.ids = ids
         self.metadatas = metadatas if metadatas is not None else []
         self.vector_count = self.embeddings.shape[0]
+        self.metric = metric
+        self.ef_construction = ef_construction
+        self.M = M
 
-        index = hnswlib.Index(space=metric, dim=self.dimension)
-        index.init_index(max_elements=self.vector_count, ef_construction=ef_construction, M=16)
+        self._build_index()
 
+    def _build_index(self):
+        index = hnswlib.Index(space=self.metric, dim=self.dimension)
+        index.init_index(max_elements=self.vector_count+100, ef_construction=self.ef_construction, M=self.M)
+        index.add_items(self.embeddings, self.ids)
+        self.index = index
+    
+    def add(self, id: int, vector: np.array, metadata: Dict = None):
+        
+        # Handle input validation
+        if not isinstance(vector, (np.ndarray, list)):
+            raise ValueError("Vector should be a NumPy array or a list.")
+        
+        vector = vector if isinstance(vector, np.ndarray) else np.array(vector)
+
+        # By default the Embedder module returns a 2D Array, so we need to handle that case
+        if vector.ndim != 1 and len(vector[0]) == self.dimension:
+            vector = vector[0]
+
+        if id in self.ids:
+            raise ValueError(f"ID {id} already exists in the index.")
+
+        if vector.ndim != 1:
+            raise ValueError("Input vector must be 1-dimensional.")
+
+        if vector.size != self.dimension:
+            raise ValueError(
+                f"Vector dimension ({vector.size}) does not match index dimension ({self.dimension})."
+        )
+
+        self.index.add_items(vector, id)
+        self.metadatas.append(metadata) if metadata else None
+        self._update_vector_count()
+        self._build_index()
+
+    def search(self, query_vector: np.ndarray, top_k: int, filter_param: Dict = None):
+        if top_k <= 0:
+            raise ValueError("Top K must be greater than 0")
+
+        if query_vector.ndim != 1:
+            raise ValueError("Input vector must be 1-dimensional.")
+
+        if query_vector.size != self.dimension:
+            raise ValueError(f"Vector dimension ({query_vector.size}) does not match index dimension ({self.dimension}).")
+
+        labels, distances = self.index.knn_query(query_vector, k=top_k)
+        top_k_indices = labels[0]
+        return top_k_indices, distances
+    
+
+        # TODO: Return formatted results
+
+        
