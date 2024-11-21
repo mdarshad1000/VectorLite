94556063e02616aaa9d512557146e835c74ffbcc
Mohammad Arshad <mdarshad1000@gmail.com>
2024-11-22 04:29:04 +0530

    feat: Auto-increment max_element and rebuild index

--- index/hnsw_index.py
+++ index/hnsw_index.py
@@ -1,6 +1,6 @@
 import hnswlib
 import numpy as np
-from typing import List, Dict
+from typing import List, Dict, Union
 from index.abstract_index import AbstractIndex
 
 # TODO: Include Metadata filtering (post and pre filtering)
@@ -23,7 +23,7 @@ class HNSWIndex(AbstractIndex):
         ef_construction (int, optional): The number of nearest neighbors to consider during the graph construction. Default is 100.
         metric (str, optional): The distance metric to use for the HNSW algorithm. Default is "cosine". Other possible options are 'l2' and 'ip'
     '''
-    def __init__(self, table_name: str, dimension: int, ids: list, embeddings: np.array, metadatas: List[Dict] = None, M: int = 16, ef_construction: int = 100, metric: str = "cosine"):
+    def __init__(self, table_name: str, dimension: int, ids: list, embeddings: np.ndarray, metadatas: List[Dict] = None, M: int = 16, ef_construction: int = 100, metric: str = "cosine", max_elements: int = None):
         super().__init__(table_name, "HNSW", dimension, ids, embeddings)
 
         if not isinstance(embeddings, (np.ndarray, list)):
@@ -48,40 +48,47 @@ class HNSWIndex(AbstractIndex):
         self.metric = metric
         self.ef_construction = ef_construction
         self.M = M
+        self.max_elements = max_elements if max_elements is not None else self.vector_count
 
-        self._build_index()
+        self._build_index(max_element=self.max_elements)
 
-    def _build_index(self):
-        index = hnswlib.Index(space=self.metric, dim=self.dimension)
-        index.init_index(max_elements=self.vector_count+100, ef_construction=self.ef_construction, M=self.M)
-        index.add_items(self.embeddings, self.ids)
-        self.index = index
-    
-    def add(self, id: int, vector: np.array, metadata: Dict = None):
+    def add(self, idx: Union[int, List[int]], vector : Union[List, np.array], metadata: Union[Dict, List[Dict], None] = None):
         
+        new_size = self.vector_count + len(idx) if isinstance(idx, list) else self.vector_count + 1
+
+        if new_size > self.max_elements:
+            self._build_index(max_element=new_size)
+
         # Handle input validation
         if not isinstance(vector, (np.ndarray, list)):
-            raise ValueError("Vector should be a NumPy array or a list.")
+            raise ValueError("Vector should be a NumPy array or a list.") 
         
-        vector = vector if isinstance(vector, np.ndarray) else np.array(vector)
+        vector = vector if isinstance(vector, np.ndarray) else np.array(vector) 
+        
+        if isinstance(idx, int):
+            idx = [idx]
+            vector = vector[0] 
+            # Validate the single vector's dimensions
+            if vector.shape[0] != self.dimension:
+                raise ValueError("Vector dimension does not match index dimension.")
+        else:
+            if len(idx) != vector.shape[0]:
+                raise ValueError("Each ID must correspond to a single vector.")
+            
+            # Validate each vector's dimensions
+            if any(v.size != self.dimension for v in vector):
+                raise ValueError("One or more vectors' dimensions do not match the index dimension.")
 
-        # By default the Embedder module returns a 2D Array, so we need to handle that case
-        if vector.ndim != 1 and len(vector[0]) == self.dimension:
-            vector = vector[0]
+        # Check for existing IDs
+        if any(i in self.ids for i in idx):
+            raise ValueError(f"One or more IDs already exist in the index.")
 
-        if id in self.ids:
-            raise ValueError(f"ID {id} already exists in the index.")
+        for _ in range(len(idx)):
+            self._update_vector_count()
 
-        if vector.ndim != 1:
-            raise ValueError("Input vector must be 1-dimensional.")
-
-        if vector.size != self.dimension:
-            raise ValueError(f"Vector dimension ({vector.size}) does not match index dimension ({self.dimension}).")
-
-        self.index.add_items(vector, id)
         self.embeddings = np.vstack([self.embeddings, vector])
-        self.metadatas.append(metadata) if metadata else None
-        self._update_vector_count()
+        self.metadatas.extend(metadata) if metadata else None
+        self.index.add_items(vector, idx)
 
     def search(self, query_vector: np.ndarray, top_k: int, filter_param: Dict = None):
         if top_k <= 0:
@@ -96,7 +103,6 @@ class HNSWIndex(AbstractIndex):
         labels, distances = self.index.knn_query(query_vector, k=top_k)
         top_k_indices = labels[0]
         score = distances[0]
-
         results = [
             {
                 "id": item,
@@ -108,4 +114,9 @@ class HNSWIndex(AbstractIndex):
         ]
         return results
 
-        
+    def _build_index(self, max_element:int):
+        index = hnswlib.Index(space=self.metric, dim=self.dimension)
+        index.init_index(max_elements=max_element, ef_construction=self.ef_construction, M=self.M)
+        index.add_items(self.embeddings, self.ids)
+        self.index = index
+    
